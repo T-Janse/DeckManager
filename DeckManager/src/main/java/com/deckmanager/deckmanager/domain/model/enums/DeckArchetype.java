20b4165f224ca6577f84fad1633d90e46fccb713
T-Janse <thijsjohanjan@gmail.com>
2025-07-18 08:29:21 +0200

    updated docs to test

--- DeckManager/src/main/java/com/deckmanager/deckmanager/domain/model/enums/DeckArchetype.java
+++ DeckManager/src/main/java/com/deckmanager/deckmanager/domain/model/enums/DeckArchetype.java
@@ -41,7 +41,7 @@ public enum DeckArchetype {
     ;
     /**
      *
-     * @return Archetypetest
+     * @return Archetypetest test
      */
     public static DeckArchetype manaCostToArchetype(HashMap<Mana, Integer> manaCost) {
         Set<Mana> colorsPresent = manaCost.entrySet().stream()
