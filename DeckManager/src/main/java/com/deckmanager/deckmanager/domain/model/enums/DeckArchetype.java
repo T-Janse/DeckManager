bf81c2390062497c18accf8d6d3b8aefe810622f
T-Janse <thijsjohanjan@gmail.com>
2025-07-18 09:02:42 +0200

    testing pre-commit that adds javadoc

--- DeckManager/src/main/java/com/deckmanager/deckmanager/domain/model/enums/DeckArchetype.java
+++ DeckManager/src/main/java/com/deckmanager/deckmanager/domain/model/enums/DeckArchetype.java
@@ -41,7 +41,7 @@ public enum DeckArchetype {
     ;
     /**
      *
-     * @return testPrecom
+     * @return testprocan
      */
     public static DeckArchetype manaCostToArchetype(HashMap<Mana, Integer> manaCost) {
         Set<Mana> colorsPresent = manaCost.entrySet().stream()
